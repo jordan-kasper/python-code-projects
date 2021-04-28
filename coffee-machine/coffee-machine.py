d803a913a733c0395e4c6a70fe81ea05ca424a8d
Jordan Kasper <jordan.kasper@gmail.com>
2021-04-28 16:00:21 -0700

    updating coffee

--- coffee-machine/coffee-machine.py
+++ coffee-machine/coffee-machine.py
@@ -1,3 +1,19 @@
 # This program prints Hello, world!
 
-print('Hello, world!')
+# variables
+
+water = 100
+milk = 50
+coffee = 76
+money = 0.0
+
+coffee_order = input("What would you like: \n")
+
+if coffee_order == "report":
+    print(f'{water}ml \n {milk}ml \n {coffee}g \n $ {money} \n')
+elif coffee_order == 'latte':
+    if water >= 50 & milk >= 25 & coffee >= 22:
+        print("insert $2.50")
+
+
+print(f'Making your {coffee_order}')
