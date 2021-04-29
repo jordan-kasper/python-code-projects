4c9126845b0a446c4c7c6dc8e8e6d3221d3eb0aa
Jordan Kasper <jordan.kasper@gmail.com>
2021-04-29 08:39:40 -0700

    updating coffee functions

--- coffee-machine/coffee-machine.py
+++ coffee-machine/coffee-machine.py
@@ -6,14 +6,53 @@ water = 100
 milk = 50
 coffee = 76
 money = 0.0
+is_on = True
 
-coffee_order = input("What would you like: \n")
 
-if coffee_order == "report":
+def coffee_report():
     print(f'{water}ml \n {milk}ml \n {coffee}g \n $ {money} \n')
-elif coffee_order == 'latte':
-    if water >= 50 & milk >= 25 & coffee >= 22:
-        print("insert $2.50")
 
 
-print(f'Making your {coffee_order}')
+def refill_machine():
+    global water
+    global milk
+    global coffee
+    water = 100
+    milk = 50
+    coffee = 76
+
+    coffee_report()
+
+
+def latte_order():
+    global water
+    global milk
+    global coffee
+
+    if water >= 50 and milk >= 25 and coffee >= 22:
+        print(f'Making your {coffee_order}')
+        print("Here is your Latte, have a nice day!")
+        water -= 50
+        milk -= 25
+        coffee -= 23
+        coffee_report()
+
+
+def mocha_order():
+    if water >= 50 and milk >= 25 and coffee >= 22:
+        print(f'Making your {coffee_order}')
+        print("Here is your mocha, have a nice day!")
+
+
+while is_on:
+
+    coffee_order = input("What would you like: \n")
+
+    if coffee_order == "report":
+        coffee_report()
+    elif coffee_order == 'latte':
+        latte_order()
+    elif coffee_order == 'mocha':
+        mocha_order()
+    elif coffee_order == 'refill':
+        refill_machine()
